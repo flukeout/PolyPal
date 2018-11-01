dbad9d95f00530f998e9e980244ba15edf08e3e0
mozilla <luke.pacholski@gmail.com>
2018-10-31 22:04:40 -0700

    Crappy download function is ready

--- js/listeners.js
+++ js/listeners.js
@@ -16,3 +16,21 @@ setExtrudeMode("line");
 document.querySelectorAll(".extrude").forEach((el) => {
   el.addEventListener("click", (e) => setExtrudeMode(e.target.getAttribute("type")));
 });
+
+
+document.querySelector(".download").addEventListener("click", () => {
+  downloadSvg();
+});
+
+
+function downloadSvg() {
+  var svgData = dQ(".svg-image").outerHTML;
+  var svgBlob = new Blob([svgData], {type:"image/svg+xml;charset=utf-8"});
+  var svgUrl = URL.createObjectURL(svgBlob);
+  var downloadLink = document.createElement("a");
+  downloadLink.href = svgUrl;
+  downloadLink.download = "polypal.svg";
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+}
