2501cb5f65874cb798e47c147ba992e4a8c55f2b
mozilla <luke.pacholski@gmail.com>
2018-11-04 10:49:30 -0800

    keyboard based tool selector

--- js/keys.js
+++ js/keys.js
@@ -8,15 +8,23 @@ const keyMap = {
   8  : "delete",
   187 : "plus",
   189 : "minus",
+  83 : "selector",
+  66 : "paintbrush",
+  77 : "move",
 }
 
 const getKey = keyCode => {
+  console.log(keyCode);
   return keyMap[keyCode];
 }
 
 window.addEventListener("keydown", e => {
 
   let key = getKey(e.keyCode);
+
+  if(key == "selector" || key == "paintbrush" || key == "move") {
+    selectTool(key);
+  }
 
   if(key == "delete") {
     points = customFilter(points, (p => p.selected));
